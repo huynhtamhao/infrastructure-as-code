5931f5729ae5045fc5e63f245c7f8a326f1fb692
huynhtamhao <huynhtamhao@gmail.com>
2020-12-28 17:42:00 +0700

    add POP-OS script

--- PopOS/init.sh
+++ PopOS/init.sh
@@ -78,12 +78,12 @@ sudo apt install -y code # or code-insiders
 sudo mkdir /usr/lib/jvm
 # Download file
 wget https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u275-b01/OpenJDK8U-jdk_x64_linux_hotspot_8u275b01.tar.gz
-sudo tar -xvzf ./OpenJDK8U-jdk_x64_linux_hotspot_8u275b01.tar.gz ~/usr/lib/jvm/openjdk-8
+sudo tar -xvzf ./OpenJDK8U-jdk_x64_linux_hotspot_8u275b01.tar.gz && mv jdk8u275-b01 ~/usr/lib/jvm/openjdk-8
 export JAVA_HOME=/usr/lib/jvm/openjdk-8
 export PATH=$PATH:$JAVA_HOME/bin
 export PATH=$PATH:$JAVA_HOME/jre/bin
 wget https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.9.1+1/OpenJDK11U-jdk_x64_linux_hotspot_11.0.9.1_1.tar.gz
-sudo tar -xvzf ./OpenJDK11U-jdk_x64_linux_hotspot_11.0.9.1_1.tar.gz ~/usr/lib/jvm/openjdk-11
+sudo tar -xvzf ./OpenJDK10U-jdk_x64_linux_hotspot_11.0.9.1_1.tar.gz && mv jdk-11.0.9.1+1 ~/usr/lib/jvm/openjdk-11
 
 
 sudo sh ./devops.sh
