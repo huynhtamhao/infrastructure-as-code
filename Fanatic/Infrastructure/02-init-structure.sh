f6077c6f0bb337f686d2de76faf9c4e1b0ef45f7
HaoHuynh <huynhtamhao@gmail.com>
2020-12-29 17:03:20 +0700

    init file

---
+++ Fanatic/Infrastructure/02-init-structure.sh
@@ -0,0 +1,16 @@
+sh init-new-project.sh 01_user all
+sh init-new-project.sh 02_material all
+sh init-new-project.sh 03_product all
+sh init-new-project.sh 04_procurement all
+sh init-new-project.sh 05_maintenance all
+sh init-new-project.sh 06_productdata all
+sh init-new-project.sh 07_datawarehouse all
+sh init-new-project.sh master all
+
+sh init-new-project.sh admin backend
+sh init-new-project.sh config backend
+sh init-new-project.sh discovery backend
+sh init-new-project.sh fanaman backend
+sh init-new-project.sh gateway backend
+
+sh init-new-project.sh menu frontend
