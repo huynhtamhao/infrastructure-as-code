6311d8d360108a45ddc4396d98574b92626a8299
huynhtamhao <huynhtamhao@gmail.com>
2020-12-29 13:18:09 +0700

    add POP-OS script

--- PopOS/init.sh
+++ PopOS/init.sh
@@ -82,7 +82,7 @@ echo "================================================== INSTALL DOCKER-COMPOSE 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-sudo usermod -aG docker posos
+sudo usermod -aG docker hao
 
 
 echo "================================================== INSTALL DOCKSTATION =================================================="
