f0c87621aa6004df6afb19399b4637e56b0d2abb
Hao Huynh <hao@pop-os.localdomain>
2021-01-02 15:23:52 +0700

    change config

--- PopOS/copy-config.sh
+++ PopOS/copy-config.sh
@@ -4,5 +4,4 @@ sudo cp ./SpaceVim/init.toml ~/.SpaceVim.d/
 sudo cp ./Fish/config.fish ~/.config/fish/
 
 sudo cp ./Fish/docker.fish ~/.config/fish/conf.d/
-sudo cp ./Fish/fish-ssh-agent.fish ~/.config/fish/conf.d/
 sudo cp ./Fish/fortune.fish ~/.config/fish/conf.d/
