f76b73d284975329bf2363d5bdae543a2299ac53
Hao Huynh <hao@pop-os.localdomain>
2021-01-03 15:40:12 +0700

    change config

--- PopOS/font-config.sh
+++ PopOS/font-config.sh
@@ -4,8 +4,8 @@ wget https://download.jetbrains.com/fonts/JetBrainsMono-2.221.zip
 unzip JetBrainsMono-2.221.zip -d ./JetBrainsMono
 cp JetBrainsMono/fonts/ttf/*.ttf ~/.local/share/fonts/
 
-wget https://github.com/tonsky/FiraCode/releases/download/5.2/Fira_Code_v5.2.zip
-unzip Fira_Code_v5.2.zip -d ./FiraCode
-cp FiraCode/ttf/*.ttf ~/.local/share/fonts/
+#wget https://github.com/tonsky/FiraCode/releases/download/5.2/Fira_Code_v5.2.zip
+#unzip Fira_Code_v5.2.zip -d ./FiraCode
+#cp FiraCode/ttf/*.ttf ~/.local/share/fonts/
 
 fc-cache -f -v
