847315dc979500c400ddce645b8188ee8023a5e6
hao <huynhtamhao@gmail.com>
2021-01-09 10:53:16 +0700

    init new structrue

--- Fanatic/Infrastructure/template-project/02-init-structure.sh
+++ Fanatic/Infrastructure/template-project/02-init-structure.sh
@@ -1,20 +1,17 @@
 SERVER_URL="192.168.210.201"
 
 sh init-new-project.sh 01_user $SERVER_URL
-
-# sh init-new-project.sh 01_user all $SERVER_URL
-# sh init-new-project.sh 02_material all $SERVER_URL
-# sh init-new-project.sh 03_product all $SERVER_URL
-# sh init-new-project.sh 04_procurement all $SERVER_URL
-# sh init-new-project.sh 05_maintenance all $SERVER_URL
-# sh init-new-project.sh 06_productdata all $SERVER_URL
-# sh init-new-project.sh 07_datawarehouse all $SERVER_URL
-# sh init-new-project.sh master all $SERVER_URL
+sh init-new-project.sh 02_material $SERVER_URL
+sh init-new-project.sh 03_product $SERVER_URL
+sh init-new-project.sh 04_procurement $SERVER_URL
+sh init-new-project.sh 05_maintenance $SERVER_URL
+sh init-new-project.sh 06_productdata $SERVER_URL
+sh init-new-project.sh 07_datawarehouse $SERVER_URL
+sh init-new-project.sh master $SERVER_URL
 
 # sh init-new-project.sh admin backend $SERVER_URL
 # sh init-new-project.sh config backend $SERVER_URL
 # sh init-new-project.sh discovery backend $SERVER_URL
 # sh init-new-project.sh fanaman backend $SERVER_URL
 # sh init-new-project.sh gateway backend $SERVER_URL
-
 # sh init-new-project.sh menu frontend $SERVER_URL
