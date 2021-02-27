3eba7c810e26d4d4e395694cd39613ef0a1472f7
Unknown <unknown@example.com>
2021-02-27 15:42:17 +0700

    add config

--- PopOS/devops.sh
+++ PopOS/devops.sh
@@ -2,15 +2,13 @@
 # Install vitualbox
 sudo apt update
 echo "============================= INSTALL VIRTUAL BOX================================================"
-sudo apt-get install virtualbox
-sudo apt-get install virtualbox—ext–pack
-
+sudo apt install -y virtualbox
 
 # Install Vagrant
-echo "============================= INSTALL VAGRANT ============================="
-wget https://releases.hashicorp.com/vagrant/2.2.14/vagrant_2.2.14_x86_64.deb
-sudo dpkg -i vagrant_2.2.14_x86_64.deb
-vagrant box add ubuntu/bionic64
+# echo "============================= INSTALL VAGRANT ============================="
+# wget https://releases.hashicorp.com/vagrant/2.2.14/vagrant_2.2.14_x86_64.deb
+# sudo dpkg -i vagrant_2.2.14_x86_64.deb
+# vagrant box add ubuntu/bionic64
 
 echo "============================= INSTALL ANSIBLE ============================="
 # Install ansible
@@ -23,7 +21,8 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sudo apt-get install -y kubectl
-
+sudo apt upgrade -y 
+sudo apt autoremove -y
 
 echo "============================= INSTALL MINIKUBE ============================="
 # Mini kube
@@ -31,8 +30,8 @@ curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest
 sudo dpkg -i minikube_latest_amd64.deb
 
 
-echo "============================= INSTALL LENS ============================="
-sudo snap install kontena-lens --classic
+# echo "============================= INSTALL LENS ============================="
+# sudo snap install kontena-lens --classic
 
 
 echo "============================= INSTALL TERRAFORM ============================="
