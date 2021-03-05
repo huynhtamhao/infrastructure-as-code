f3a0e241f27d5f5fe00549830609ca61423f463a
Hao Huynh <huynhtamhao@gmail.com>
2021-03-05 08:58:58 +0700

    Copy Config for chrome remote desktop

--- PopOS/copy-config.sh
+++ PopOS/copy-config.sh
@@ -10,6 +10,8 @@ gh repo clone dexpota/kitty-themes ~/.config/kitty/kitty-themes
 ln -s ~/.config/kitty/kitty-themes/themes/ayu_mirage.conf ~/.config/kitty/theme.conf
 sudo cp ./Kitty/kitty.conf ~/.config/kitty/
 
+# Follow this url to config for linux OS (https://support.google.com/chrome/answer/1649523#linux-crd)
+sudo cp ./ChromeRemoteDesktop/.chrome-remote-desktop-session $HOME/
 # sudo cp ./Fish/fortune.fish ~/.config/fish/conf.d/
 
 # sudo mkdir -p  $HOME/.config/alacritty/ && sudo cp Alacritty/alacritty.yml $HOME/.config/alacritty/
