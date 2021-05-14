062d199eb2fcb796a61c3db650b20d6667e3a708
huynhtamhao <huynhtamhao@gmail.com>
2021-05-14 17:11:38 +0700

    change

--- PopOS/environment.sh
+++ PopOS/environment.sh
@@ -1,30 +1,27 @@
 echo "============================= INSTALL DOCKER ======================================"
 # Install docker (Lasted Version)
-sudo apt update
-sudo apt-get install -y \
+sudo apt-get -y install \
     apt-transport-https \
     ca-certificates \
     curl \
-    gnupg-agent \
-    software-properties-common
+    gnupg \
+    lsb-release
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-sudo add-apt-repository -y \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Install with lasted version
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
-
-echo "============================= INSTALL DOCKER-COMPOSE =============================="
+echo "============================= INSTALL DOCKER-COMPOSE ============================="
 # Docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
 sudo usermod -aG docker hao
 
 sudo apt update
@@ -40,9 +37,13 @@ sudo mkdir /usr/lib/jvm
 #sudo tar -xvzf ./OpenJDK8U-jdk_x64_linux_hotspot_8u275b01.tar.gz && sudo mv jdk8u275-b01 /usr/lib/jvm/openjdk-8
 #wget https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.9.1+1/OpenJDK11U-jdk_x64_linux_hotspot_11.0.9.1_1.tar.gz
 #sudo tar -xvzf ./OpenJDK11U-jdk_x64_linux_hotspot_11.0.9.1_1.tar.gz && sudo mv jdk-11.0.9.1+1 /usr/lib/jvm/openjdk-11
-wget https://download.java.net/java/GA/jdk16/7863447f0ab643c585b9bdebf67c69db/36/GPL/openjdk-16_linux-x64_bin.tar.gz
-sudo tar -xvzf ./openjdk-16_linux-x64_bin.tar.gz
-sudo mv jdk-16 /usr/lib/jvm/
+# wget https://download.java.net/java/GA/jdk16/7863447f0ab643c585b9bdebf67c69db/36/GPL/openjdk-16_linux-x64_bin.tar.gz
+# sudo tar -xvzf ./openjdk-16_linux-x64_bin.tar.gz
+# sudo mv openjdk-16 /usr/lib/jvm/
+
+wget https://download.java.net/java/GA/jdk15.0.2/0d1cfde4252546c6931946de8db48ee2/7/GPL/openjdk-15.0.2_linux-x64_bin.tar.gz
+sudo tar -xvzf ./openjdk-15.0.2_linux-x64_bin.tar.gz
+sudo mv jdk-15.0.2 /usr/lib/jvm/openjdk-15.0.2
 
 
 # echo "============================= INSTALL NODEJS ============================="
--- PopOS/terminal.sh
+++ PopOS/terminal.sh
@@ -20,7 +20,7 @@ curl -fsSL https://starship.rs/install.sh | bash
 # Colorls
 # sudo gem install colorls
 
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
